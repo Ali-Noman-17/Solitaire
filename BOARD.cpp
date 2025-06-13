e90924342c8ec773059c258737222670a4bf2304
Ali-Noman-17 <bscs24018@itu.edu.pk>
2025-06-13 11:57:19 +0500

    Board Class (master octopus)

---
+++ BOARD.cpp
@@ -0,0 +1,113 @@
+#include "BOARD.h"
+
+Board::Board() : randoms(new int[NUMBER_CARDS]), num(NUMBER_CARDS), score(0), 
+play(ALTERNATE_DIVISION_NUM, ALTERNATE_DIVISION_TEXTURE), suits(SUIT_DIVISION_NUM, SUIT_DIVISION_TEXTURE), 
+pool(POOL_DIVISION_NUM, POOL_DIVISION_TEXTURE),
+deck(new Card* [NUMBER_CARDS]) {
+}
+
+Board::~Board() { 
+	for (int i = 0; i < NUMBER_CARDS; i++) { delete deck[i]; }
+	delete[] deck;
+	delete[] randoms;
+}
+
+void Board::initDeck() {
+	int preset = 0;
+	for (int i = 0; i < NUMBER_VALS; i++) {
+		string path = pathConctructor(CARD_VALUES[i], SUIT_SPADES);
+		deck[preset + i] = new Spades(i + 1, CARD_VALUES[i], path.data());
+	}
+	preset += NUMBER_VALS;
+	for (int i = 0; i < NUMBER_VALS; i++) {
+		string path = pathConctructor(CARD_VALUES[i], SUIT_CLUBS);
+		deck[preset + i] = new Clubs(i + 1, CARD_VALUES[i], path.data());
+	}
+	preset += NUMBER_VALS;
+	for (int i = 0; i < NUMBER_VALS; i++) {
+		string path = pathConctructor(CARD_VALUES[i], SUIT_HEARTS);
+		deck[preset + i] = new Hearts(i + 1, CARD_VALUES[i], path.data());
+	}
+	preset += NUMBER_VALS;
+	for (int i = 0; i < NUMBER_VALS; i++) {
+		string path = pathConctructor(CARD_VALUES[i], SUIT_DIAMONDS);
+		deck[preset + i] = new Diamonds(i + 1, CARD_VALUES[i], path.data());
+	}
+}
+
+void Board::initPlay() {
+	for (int i = 0; i < ALTERNATE_DIVISION_NUM; i++) {
+		for (int j = 0; j < i + 1; j++) {
+			int index = getRandom();
+			play.addToCell(deck[index], i);
+		}
+	}
+}
+
+void Board::initSuits() {}
+
+void Board::initPool() {
+	int size = num;
+	for (int i = 0; i < size; i++) {
+		int index = getRandom();
+		pool.addToCell(deck[index], POOL_LOCKED_CELL);
+	}
+}
+
+void Board::initialise() {
+	initDeck();
+	initPlay();
+	initPool();
+	initSuits();
+}
+
+void Board::position() {
+	positionPool();
+	positionSuits();
+	positionPlay();
+}
+
+void Board::positionPlay() {
+	float y = (2 * VERT_WIDTH) * CELL_WIDTH;
+	play.move(0, y);
+}
+
+void Board::positionSuits() {
+	float x = (3 * SPACER_WIDTH) + (3 * CELL_WIDTH);
+	suits.move(x, 0);
+}
+
+void Board::positionPool() {
+	pool.move(0, 0);
+}
+
+void Board::addScore() { score += SCORE_INCREMENT; }
+
+bool Board::checkWin() { 
+	for (int i = 0; i < NUMBER_SUITS; i++) {
+		if (suits.getCellNum(i) < NUMBER_CARDS) return 0;
+	}
+	return 1;
+}
+
+void Board::randomise() {
+	for (int i = 0; i < NUMBER_CARDS; i++) {
+		randoms[i] = i;
+	}
+	for (int i = NUMBER_CARDS - 1; i > 0; i--) {
+		int j = GetRandomValue(0, i);
+		std::swap(randoms[i], randoms[j]);
+	}
+}
+
+int Board::getRandom() {
+	int random = randoms[num];
+	num--;
+	return random;
+}
+
+string Board::pathConctructor(const char name, const char suit) {
+	string dash = "-";
+	string path = FRONT_TEXTURE_PREFIX + name + dash + FRONT_TEXTURE_SUFFIX;
+	return  path;
+}
---
+++ BOARD.h
@@ -0,0 +1,55 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <iostream>
+#include <string>
+#include <raylib.h>
+#include "DIVISION.h"
+using namespace std;
+
+#define NUMBER_VALS 13
+#define NUMBER_SUITS 4
+#define NUMBER_CARDS 52
+#define CARD_VALUES "A23456789TJQK"
+#define SCORE_INCREMENT 5
+#define BOARD_LENGTH 1000
+#define BOARD_HEIGHT 800
+
+class Board {
+	static Board* instance;
+	int* randoms;
+	int num;
+	int score;
+	Division<AlternateCell> play;
+	Division<SuitCell> suits;
+	Division<FreeCell> pool;
+	Card** deck;
+	Board();
+	~Board();
+	Board(const Board& obj) = delete;
+	Board& operator=(const Board& obj) = delete;
+public:
+	static Board& getInstance() {
+		if (instance == nullptr) instance = new Board();
+		return *instance;
+	}
+	
+	void initialise();
+	void initDeck();
+	void initPlay();
+	void initSuits();
+	void initPool();
+
+	void position();
+	void positionPlay();
+	void positionSuits();
+	void positionPool();
+
+	void addScore();
+	bool checkWin();
+	void randomise();
+	int getRandom();
+	string pathConctructor(const char name, const char suit);
+};
+
+#endif BOARD_H
